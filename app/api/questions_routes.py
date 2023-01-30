427bae533faa5ae2f13b6f97983e2993930e39aa
Tadd JV <taddjv111@gmail.com>
2023-01-29 23:08:19 -0500

    question form done

--- app/api/questions_routes.py
+++ app/api/questions_routes.py
@@ -1,9 +1,19 @@
 from flask import Blueprint,request
-from sqlalchemy import insert,update
 from app.models import Question,db
+from app.forms import QuestionForm
 
 
 questions_routes = Blueprint("questions", __name__)
+
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f'{field} : {error}')
+    return errorMessages
 
 @questions_routes.route("/<int:id>", methods=["GET"])
 def get_question(id):
@@ -44,12 +54,21 @@ def get_all_questions():
     return final
 
 
-#! We can't test this route yet because we have no form
+#! We can't test this route yet
 @questions_routes.route("/", methods=["POST"])
 def post_question():
 
-    desired_question_data = request.form
-    desired_question = Question(question=desired_question_data.question,detail=desired_question_data.detail,url=desired_question_data.url,user_id=desired_question_data.user_id)
-    db.session.add(desired_question)
-    db.session.commit()
-    return desired_question.to_dict()
+    form = QuestionForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        desired_question = Question(question=form.data['question'],detail=form.data['detail'],url=form.data["url"],user_id=None)
+        db.session.add(desired_question)
+        db.session.commit()
+        return desired_question.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+    # desired_question_data = request.form
+    # desired_question = Question(question=desired_question_data.question,detail=desired_question_data.detail,url=desired_question_data.url,user_id=desired_question_data.user_id)
+    # db.session.add(desired_question)
+    # db.session.commit()
+    # return desired_question.to_dict()
