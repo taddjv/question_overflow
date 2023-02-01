9fe584a1e4ef851fb5f92be19844aa6ac5383509
Tadd JV <taddjv111@gmail.com>
2023-02-01 18:32:51 -0500

    search routes

---
+++ app/api/search_routes.py
@@ -0,0 +1,34 @@
+from flask import Blueprint,request
+from app.models import Question,Search,db
+from app.forms import SearchForm
+from flask_login import current_user, login_required
+
+search_routes = Blueprint("searches", __name__)
+
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f'{field} : {error}')
+    return errorMessages
+
+
+#!didn't test this route because i'm not sure how to login on postman
+@search_routes.route("/questions/<query>", methods=["GET"])
+def get_results(query):
+    if (current_user.is_authenticated):
+        desired_search = Search(Search=query,user_id=current_user.id)
+        db.session.add(desired_search)
+        db.session.commit()
+    results = Question.query.filter(Question.question.contains(query))
+    final = {"results": [result.to_dict() for result in results]}
+    return final
+
+@search_routes.route("/user/<int:id>", methods=["GET"])
+def get_user_searches(id):
+    results = Search.query.filter(Search.user_id == id)
+    final = {"results": [result.to_dict() for result in results]}
+    return final
