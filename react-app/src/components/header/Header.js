341a2012aa7b64448fc3e528858060fcd1ea7482
hensell de la rosa <henseldelarosa@DESKTOP-DOB1QI3.localdomain>
2023-02-04 13:02:48 -0500

    homebutton

--- react-app/src/components/header/Header.js
+++ react-app/src/components/header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Header.css'
-import { useSelector } from 'react-redux'
-import { NavLink } from 'react-router-dom'
+import {useSelector} from 'react-redux'
+import {NavLink} from 'react-router-dom'
 
 
 import LogoutMenu from './logout/LogoutMenu'
@@ -11,63 +11,57 @@ function Header() {
   const sessionUser = useSelector(state => state.session.user)
   let currentSession;
 
-if (sessionUser){
-  currentSession = (
-    <div className='header'>
+  if (sessionUser) {
+    currentSession = (
+      <div className='header'>
 
-    <div className='header_left'>
-      {/* <NavLink className='header_homeLink' to='/'> */}
-      <h1 className='header_question'>Question</h1>
-      <h1 className='header_overflow'><em>overflow</em></h1>
-      {/* </NavLink> */}
-    </div>
+        <div className='header_left'>
+          <NavLink to='/'>
+            <h1 className='header_question'>Question</h1>
+            <h1 className='header_overflow'><em>overflow</em></h1>
+          </NavLink>
+        </div>
 
-  <div className='header_center'>
-  <i className="fa-solid fa-magnifying-glass"></i>
-    <input
-    className='search_input'
-    type='text'
-    placeholder='search...'
-    />
+        <div className='header_center'>
+          <i className="fa-solid fa-magnifying-glass"></i>
+          <input className='search_input' type='text' placeholder='search...'/>
 
-  </div>
+        </div>
 
-  <div className='header_right'>
-  <LoginMenu/>
+        <div className='header_right'>
+          <LoginMenu/>
 
-  </div>
-</div>
-)
-}else{
-  currentSession =(
-  <div className='header'>
+        </div>
+      </div>
+    )
+  } else {
+    currentSession = (
+      <div className='header'>
 
-    <div className='header_left'>
-      {/* <NavLink className='header_homeLink' to='/'> */}
-      <h1 className='header_question'>Question</h1>
-      <h1 className='header_overflow'><em>overflow</em></h1>
-      {/* </NavLink> */}
-    </div>
+        <div className='header_left'>
+          <NavLink className='header_homeLink' to='/'>
+          <h1 className='header_question'>Question</h1>
+          <h1 className='header_overflow'>
+            <em>overflow</em>
+          </h1>
+          </NavLink>
+          </div>
 
-  <div className='header_center'>
-  <i className="fa-solid fa-magnifying-glass"></i>
-    <input
-    className='search_input'
-    type='text'
-    placeholder='search...'
-    />
+        <div className='header_center'>
+          <i className="fa-solid fa-magnifying-glass"></i>
+          <input className='search_input' type='text' placeholder='search...'/>
 
-  </div>
+        </div>
 
-  <div className='header_right'>
-  <LogoutMenu/>
+        <div className='header_right'>
+          <LogoutMenu/>
 
-  </div>
-</div>
-  )
-}
-
-  return (currentSession)
+        </div>
+      </div>
+    )
   }
+
+  return(currentSession)
+}
 
 export default Header
