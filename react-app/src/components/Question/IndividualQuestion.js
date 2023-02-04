8f8743677e44968c604c11893a0ecff48a53be32
Tadd JV <taddjv111@gmail.com>
2023-02-04 12:45:22 -0500

    css work

--- react-app/src/components/Question/IndividualQuestion.js
+++ react-app/src/components/Question/IndividualQuestion.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import * as userActions from "../../store/user";
 
+import DeleteIcon from "@mui/icons-material/Delete";
+
 function IndividualQuestion({
   questionTitle,
   detail,
@@ -18,7 +20,9 @@ function IndividualQuestion({
 
   const userContent = user.user ? (
     <>
-      <div className="q-c-a-user">by {user.user.username}</div>
+      <div className="q-c-a-user">
+        by <span className="q-c-a-user1">{user.user.username}</span>
+      </div>
       <div className="q-c-a-date">{dateCreated}</div>
     </>
   ) : null;
@@ -27,7 +31,9 @@ function IndividualQuestion({
     //!add new route to answers to count the answers per question
     <>
       <div className="question-content">
-        <div className="q-c-logo"></div>
+        <div className="q-c-logo">
+          <svg data-testid="DeleteIcon"></svg>
+        </div>
         <div className="q-c-title-">
           <div className="q-c-t-q">{questionTitle}</div>
           <div className="q-c-t-d">{detail}</div>
