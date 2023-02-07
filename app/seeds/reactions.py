ff405737596b726071d14b9a55d05f3ee3def7cd
jzhang319 <jzhang319@gmail.com>
2023-02-07 08:56:59 -0500

    added more seed data - reactions

--- app/seeds/reactions.py
+++ app/seeds/reactions.py
@@ -3,9 +3,17 @@ from app.models import db, Answer, environment, SCHEMA, Reaction
 
 def seed_reactions():
     demo = Reaction(
-        answer_id=1, user_id=1, up_vote=True
+        answer_id=1, user_id=2, up_vote=True
+    )
+    demo1 = Reaction(
+        answer_id=1, user_id=2, down_vote=True
+    )
+    demo2 = Reaction(
+        answer_id=2, user_id=1, down_vote=True
     )
     db.session.add(demo)
+    db.session.add(demo1)
+    db.session.add(demo2)
     db.session.commit()
 
 
