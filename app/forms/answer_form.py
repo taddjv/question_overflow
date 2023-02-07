a10952900de5b05d5427c252273225945aed2a95
jack <jzhang319@gmail.com>
2023-02-07 11:32:38 -0500

    updated answer form validation again with more image types

--- app/forms/answer_form.py
+++ app/forms/answer_form.py
@@ -8,7 +8,7 @@ def valid_picture(form, field):
     # checks if the picture is a .png or .jpg
     picture_url = field.data
     # print(picture_url, " <-----")
-    if picture_url.lower().endswith(('.png', '.jpg', '.jpeg')):
+    if picture_url.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp', '.tif', '.tiff')):
         return {'message': 'image added successfully'}
     else:
         raise ValidationError("Not a valid image.")
