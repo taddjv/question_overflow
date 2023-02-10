4c277bb4103e1431188e17fdb52a21054d736656
jack <jzhang319@gmail.com>
2023-02-09 22:48:11 -0500

    updated

--- react-app/src/components/answer/IndividualAnswer.js
+++ react-app/src/components/answer/IndividualAnswer.js
@@ -24,6 +24,8 @@ function IndividualAnswers({
 }) {
   const dispatch = useDispatch();
 
+  const reaction = useSelector((state) => state.reactionsReducer.reaction);
+
   const [editAnswer, setEditAnswer] = useState(false);
   const [answerDetail, setAnswerDetail] = useState("");
   const [answerUrl, setAnswerUrl] = useState("");
@@ -43,6 +45,11 @@ function IndividualAnswers({
       });
   };
 
+  const handleUpvote = () => {
+    dispatch(reactionActions.postTheUpvote(answer.id));
+  };
+  useEffect(() => {}, [answer.id]);
+
   return (
     <div className="reply_container">
       <div className="answer_user_info">
@@ -55,7 +62,7 @@ function IndividualAnswers({
           <div className="upvotes">
             {getVotes(reactions).up_votes}
             <div className="up_icon">
-              <i class="fa-solid fa-thumbs-up" />
+              <i class="fa-solid fa-thumbs-up" onClick={handleUpvote} />
             </div>
             {/* end of upvote div */}
           </div>
@@ -82,7 +89,7 @@ function IndividualAnswers({
       {answer?.user?.username === user?.username && (
         <div className="ans-crud-options">
           <button
-            className="indivdual_edit_button"
+            className="individual_edit_button"
             onClick={() => {
               setEditAnswer(true);
             }}
